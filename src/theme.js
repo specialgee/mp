ed0606d53bf6a01ec15aac13edb5747148333f07
gllm <guillaume.piccarreta@gmail.com>
2020-07-17 18:21:34 +0200

    updated theme

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,31 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
+// import AbrilFatface from './assets/font/AbrilFatface-Regular.ttf';
+
+// const abrilFatface = {
+//   fontFamily: 'AbrilFatface',
+//   fontStyle: 'normal',
+//   fontDisplay: 'swap',
+//   fontWeight: 400,
+//   src: `
+//     local('AbrilFatface'),
+//     local('AbrilFatface-Regular'),
+//     url(${AbrilFatface}) format('ttf')
+//   `,
+//   unicodeRange:
+//     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+// };
+
+
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#0048ff',
     },
     secondary: {
-      main: '#19857b',
+      main: '#006eff',
     },
     error: {
       main: red.A400,
@@ -17,6 +34,16 @@ const theme = createMuiTheme({
       default: '#fff',
     },
   },
+  // typography: {
+  //   fontFamily: 'AbrilFatface, Arial',
+  // },
+  // overrides: {
+  //   MuiCssBaseline: {
+  //     '@global': {
+  //       '@font-face': [abrilFatface],
+  //     },
+  //   },
+  // },
 });
 
 export default theme;
